d4ad58662d24e431c5ad949d27fde882d2e3d13b
MegaIng1 <trampchamp@hotmail.de>
2019-01-11 14:36:40 +0100

    Added comments

--- step 1/parser.py
+++ step 1/parser.py
@@ -1,10 +1,10 @@
 from lark import Lark
 
-parser = Lark(open("grammar.lark"))
+parser = Lark(open("grammar.lark"))  # Load grammar. You can also hardcode the grammar into the sourcecode
 
-tree = parser.parse("2*3+(5/(7-3**2))")
+tree = parser.parse("2*3+(5/(7-3**2))")  # Let lark parse and create a tree
 
-print(tree.pretty())
+print(tree.pretty())  # Print the tree with a bit of indentation
 
 # # Uncomment if you have pydot installed (or install it to have a nice visual overview)
 # from lark.tree import pydot__tree_to_png
